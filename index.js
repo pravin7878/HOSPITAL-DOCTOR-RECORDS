a746078ce0552926dd1434469dd7f3d606a21bfd
pravin7878 <143276463+pravin7878@users.noreply.github.com>
2023-11-15 08:59:10 +0530

    updet

--- index.js
+++ index.js
@@ -1,46 +1,71 @@
-let name = document.getElementById('name');
-let DoctorID = document.getElementById('docID');
-let department = document.getElementById('dept');
-let exp = document.getElementById('exp');
-let email = document.getElementById('email');
-let mbl = document.getElementById('mbl');
+let submit = document.getElementById('btn')
 let myform = document.querySelector('form')
-let submit = document.querySelector('btn')
-let tbody = document.querySelector('tbody')
 
-let alldata = [];
-myform.addEventListener('submit',function(e){
-e.preventDefault();
-let data = {};
-     data.inp1 = name.value
-     data.inp2 = DoctorID.value
-     data.inp3 = department.value
-     data.inp4 = exp.value
-     data.inp5 = email.value
-     data.inp6 = mbl.value
-    alldata.push(data);
-    // tbody.innerHTML = null
-    alldata.map((ele)=>{
-       
-        let row = document.createElement('tr')
-        let td1 = document.createElement('td')
-        let td2 = document.createElement('td')
-        let td3 = document.createElement('td')
-        let td4 = document.createElement('td')
-        let td5 = document.createElement('td')
-        let td6 = document.createElement('td')
-        // let td7 = document.createElement('td')
-        // let td8 = document.createElement('button').
-        
-        td1.innerText = ele.inp1
-        td2.innerText = ele.inp2
-        td3.innerText = ele.inp3
-        td4.innerText = ele.inp4
-        td5.innerText = ele.inp5
-        td6.innerText = ele.inp6
-    //    td7.innerText = ele.inp7
-    //     td8.innerText = "delet"
-        row.append(td1,td2,td3,td4,td5,td6)
-        tbody.append(row)
-    })
+myform.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let arr = JSON.parse(localStorage.getItem("data")) || [];
+    let name = e.target.name.value
+let id = e.target.docID.value
+let dept = e.target.dept.value
+let exp = e.target.exp.value
+let email = e.target.email.value
+let mbl = e.target.mbl.value
+    // console.log(name,id,dept,exp,email,mbl)
+    let data = {
+        name,
+        id,
+        dept,
+        exp,
+        email,
+        mbl,
+    };
+   arr.push(data)
+   localStorage.setItem("data",JSON.stringify(arr))
+//    console.log(arr)
+   showdata(arr)
 });
+
+function showdata(arr){
+      let tbody = document.querySelector('tbody')
+    //   tbody.innerHTML ==null;
+      let x = ``;
+      arr.map((ele,ind)=>{
+       x+=`
+       <tr>
+           <td>${ele.name}</td>
+           <td>${ele.id}</td>
+           <td>${ele.dept}</td>
+           <td>${ele.exp}</td>
+           <td>${ele.email}</td>
+           <td>${ele.mbl}</td>
+           <td>${ roll(ele)}</td>
+           <th><button onclick="removedata(${ind})">Delete</button></th>
+     </tr>
+       `
+    })
+    tbody.innerHTML = x
+        
+    
+};
+
+function roll(ele){
+    if(ele.exp>=5){
+        return "Senior"
+    }
+    else if(5>ele.exp && ele.exp>=2){
+        return "Junior"
+    }
+    else if(ele.exp<=1){
+        return "Fresher"
+    }
+}
+
+function removedata(el){
+    let arr = JSON.parse(localStorage.getItem("data")) || [];
+    // console.log(el,"hello")
+   arr= arr.filter((e,i)=>{
+    return i!=el
+  })
+  localStorage.setItem("data",JSON.stringify(arr))
+  showdata(arr)
+}
